d165d3a1f28cf4b4f273c60a4088996d93d36455
tahakinci <tahakinci29@gmail.com>
2023-10-17 09:51:18 +0300

    end of getting data from server

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Persons } from "./components/Persons";
 import { Filter } from "./components/Filter";
 import { PersonForm } from "./components/PersonForm";
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    const URL = "http://localhost:3001/persons";
+    axios.get(URL).then((res) => {
+      const data = res.data;
+      setPersons(data);
+    });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
